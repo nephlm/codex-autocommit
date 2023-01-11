e7d7cd9d497193d67df3c80f1c5fd47135b36528
nephlm <github@quasisemi.com>
2023-01-10 21:38:08 -0500

    Working MVP

--- extension.js
+++ extension.js
@@ -5,10 +5,94 @@ const vscode = require("vscode");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
-let lastCommitTime;
+let timeoutId;
 
 const commitAndPush = async () => {
-  console.log("in commitAndPush");
+  const gitExtensionHandle = vscode.extensions.getExtension("vscode.git");
+  if (!gitExtensionHandle.isActive) {
+    console.log("git extension not active, activating");
+    gitExtensionHandle.activate().then(() => {
+      console.log("git extension activated");
+      timeoutId = setTimeout(commitAndPush, 10 * 1000);
+    });
+    console.log("Sleeping for 10 seconds.");
+    return;
+  }
+
+  const workspaceConfig = vscode.workspace.getConfiguration("codex-autocommit");
+  console.log("Codex Autocommit active: " + workspaceConfig.get("active"));
+  console.log(
+    "Codex Autocommit interval: " +
+      workspaceConfig.get("intervalMinutes") +
+      "min"
+  );
+  if (!workspaceConfig.get("active")) {
+    console.log("Codex Autocommit is not active; Exiting");
+    return;
+  }
+
+  const api = gitExtensionHandle.exports.getAPI(1);
+  console.log("git extension state is: " + api.state);
+  if (api.state != "initialized") {
+    console.log("git extension not initialized");
+    timeoutId = setTimeout(commitAndPush, 10 * 1000);
+    console.log("Sleeping for 10 seconds");
+    return;
+  }
+
+  const setNext = function () {
+    const sleepMinutes = workspaceConfig.get("intervalMinutes");
+    timeoutId = setTimeout(commitAndPush, sleepMinutes * 60 * 1000);
+    console.log(
+      "commitAndPush complete, sleeping for " + sleepMinutes + " minutes"
+    );
+  };
+
+  const check_push = function (repository) {
+    if (repository.state.remotes.length) {
+      console.log("Pushing Changes");
+      var push_promise = repository.push();
+      push_promise.then(() => {
+        setNext();
+      });
+    } else {
+      console.log("No remote to push to.");
+      setNext();
+    }
+  };
+
+  const check_commit = function (repository) {
+    if (repository.state.indexChanges.length) {
+      console.log("Committing changes.");
+      const date = new Date();
+      const iso_string = date.toISOString();
+      var commitPromise = repository.commit(
+        iso_string + ": " + repository.inputBox.value
+      );
+      commitPromise.then(check_push(repository));
+    } else {
+      console.log("Nothing to commit.");
+      check_push(repository);
+    }
+  };
+
+  const repository = api.repositories[0];
+  await repository.status();
+
+  const modified = repository.state.workingTreeChanges.map((item) => {
+    return item.uri.path;
+  });
+  var addPromise;
+  console.log(modified);
+
+  if (modified.length) {
+    console.log("Staging Files");
+    addPromise = repository.add(modified);
+    addPromise.then(() => check_commit(repository));
+  } else {
+    console.log("Working Tree is clean.");
+    check_commit(repository);
+  }
 };
 
 /**
@@ -21,43 +105,34 @@ function activate(context) {
     'Congratulations, your extension "codex-autocommit" is now active!'
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "codex-autocommit.helloWorld",
-    function () {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from Nephlm!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
+  // Run the commit and push task every time the SCM state changes
+  // const scm = vscode.scm.activeSourceControl;
 
   const commitDisposable = vscode.commands.registerCommand(
     "codex-autocommit.commitAndPush",
     () => {
-      // commitAndPush
-      console.log("function called");
+      commitAndPush();
+      console.log("commit and push manual command called");
     }
   );
   context.subscriptions.push(commitDisposable);
 
-  // Run the commit and push task every time the SCM state changes
-  const scm = vscode.scm.activeProvider;
-  if (scm) {
-    scm.onDidChangeState(async () => {
-      console.log("onDidChangeState triggered");
-      if (!lastCommitTime || Date.now() - lastCommitTime > 1000 * 60 * 0.1) {
-        // It has been more than 10 minutes since the last commit
-        console.log("passed the date check");
-        lastCommitTime = Date.now();
-        await commitAndPush();
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration((e) => {
+      if (
+        e.affectsConfiguration("codex-autocommit.active") ||
+        e.affectsConfiguration("codex-autocommit.intervalMinutes")
+      ) {
+        console.log("Configuraton changed; restarting");
+        if (timeoutId) {
+          clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(commitAndPush, 10 * 1000);
       }
-    });
-  }
+    })
+  );
+
+  commitAndPush();
 }
 
 // This method is called when your extension is deactivated
